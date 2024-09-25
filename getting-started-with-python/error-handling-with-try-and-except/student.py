f06971d009063a53d879a54bf8db9e6d8c4760eb
Lisa Savoie <twosavoie@gmail.com>
2024-09-25 13:51:02 -0500

    Add files via upload

---
+++ getting-started-with-python/error-handling-with-try-and-except/student.py
@@ -0,0 +1,10 @@
+print("Which two numbers do you want to add together?")
+
+user_input1 = input("Input your first number: ")
+user_input2 = input("Input your second number: ")
+
+try:
+    print(float(user_input1) + float(user_input2))
+except:
+    print("At least one of your inputs is not a number")
+    
